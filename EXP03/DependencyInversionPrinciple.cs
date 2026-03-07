2cc1e41a01c2c5a7ad8a55f75d78eeff54d36770
upthemaster <upthemaster@gmail.com>
2026-03-08 01:13:36 +0530

    DependencyInversionPrinciple.cs is created

---
+++ EXP03/DependencyInversionPrinciple.cs
@@ -0,0 +1,53 @@
+﻿//namespace DIP
+//{
+
+//    // Abstraction
+//    interface IMessage
+//    {
+//        void Send();
+//    }
+
+//    // Low-level module
+//    class Email : IMessage
+//    {
+//        public void Send()
+//        {
+//            Console.WriteLine("Email Sent");
+//        }
+//    }
+
+//    class SMS : IMessage
+//    {
+//        public void Send()
+//        {
+//            Console.WriteLine("SMS Sent");
+//        }
+//    }
+
+//    // High-level module
+//    class Notification
+//    {
+//        private IMessage message;
+
+//        // Constructor injection
+//        public Notification(IMessage msg)
+//        {
+//            message = msg;
+//        }
+
+//        public void Notify()
+//        {
+//            message.Send();
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            IMessage msg = new Email();   // Can switch to SMS easily
+//            Notification notification = new Notification(msg);
+
+//            notification.Notify();
+//        }
+//    }
+//}
