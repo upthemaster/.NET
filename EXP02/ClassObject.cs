cc33cc22bec4b93085f2b04c94d3943f96c178e5
upthemaster <upthemaster@gmail.com>
2026-03-08 01:10:06 +0530

    ClassObject.cs is created

---
+++ EXP02/ClassObject.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+
+//class Student
+//{
+//    public int rollNo;
+//    public string name;
+
+//    public void Display()
+//    {
+//        Console.WriteLine("Roll No: " + rollNo);
+//        Console.WriteLine("Name: " + name);
+//    }
+//}
+
+//class Project
+//{
+//    static void Main(string[] args)
+//    {
+//        Student s1 = new Student();
+
+//        s1.rollNo = 101;
+//        s1.name = "Rahul";
+
+//        s1.Display();
+//    }
+//}
