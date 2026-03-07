49abcfb2270d548ac79cec18ba9042f6f187ac15
upthemaster <upthemaster@gmail.com>
2026-03-08 01:14:33 +0530

    LiskovSubstitutionPrinciple.cs is created

---
+++ EXP03/LiskovSubstitutionPrinciple.cs
@@ -0,0 +1,40 @@
+﻿//namespace LSP
+//{
+
+//    using System;
+//    //base class
+//    class Bird
+//    {
+//        public virtual void Move()
+//        {
+//            Console.WriteLine("Bird moves");
+//        }
+//    }
+//    //derived class
+//    class Sparrow : Bird
+//    {
+//        public override void Move()
+//        {
+//            Console.WriteLine("Sparrow flies");
+//        }
+//    }
+
+//    class Ostrich : Bird
+//    {
+//        public override void Move()
+//        {
+//            Console.WriteLine("Ostrich walks");
+//        }
+//        internal class Program
+//        {
+//            static void Main(string[] args)
+//            {
+//                Bird b1 = new Sparrow();
+//                Bird b2 = new Ostrich();
+
+//                b1.Move();
+//                b2.Move();
+//            }
+//        }
+//    }
+//}
