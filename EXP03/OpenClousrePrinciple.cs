2db8aeafeeb09f27917c4fd0ad92f09dcb0000c7
upthemaster <upthemaster@gmail.com>
2026-03-08 01:15:04 +0530

    OpenClousrePrinciple.cs is created

---
+++ EXP03/OpenClousrePrinciple.cs
@@ -0,0 +1,53 @@
+﻿//namespace OCP
+//{
+
+//    // Common interface
+//    interface IPayment
+//    {
+//        void Pay();
+//    }
+
+//    // Different implementations
+//    class CreditCardPayment : IPayment
+//    {
+//        public void Pay()
+//        {
+//            Console.WriteLine("Paid using Credit Card");
+//        }
+//    }
+
+//    class UPIPayment : IPayment
+//    {
+//        public void Pay()
+//        {
+//            Console.WriteLine("Paid using UPI");
+//        }
+//    }
+//    //If we want to add CashPayment, we just create it
+
+//    //No changes to existing code
+//    // Just extend by adding new class
+//    class CashPayment : IPayment
+//    {
+//        public void Pay()
+//        {
+//            Console.WriteLine("Paid using Cash");
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            IPayment payment;
+
+//            payment = new CreditCardPayment();
+//            payment.Pay();
+
+//            payment = new UPIPayment();
+//            payment.Pay();
+
+//            payment = new CashPayment();
+//            payment.Pay();
+//        }
+//    }
+//}
