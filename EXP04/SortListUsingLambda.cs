ca6e3d435d93801dafe875dcac18a54f7902c30b
upthemaster <upthemaster@gmail.com>
2026-03-08 01:18:01 +0530

    SortListUsingLambda.cs is created

---
+++ EXP04/SortListUsingLambda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sort_list_using_lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 1, 9, 3, 7 };
+
+            Console.WriteLine("Original List:");
+            Console.WriteLine(string.Join(", ", numbers));
+
+            // Ascending order
+            numbers.Sort((a, b) => a.CompareTo(b));
+
+            Console.WriteLine("\nSorted List (Ascending):");
+            Console.WriteLine(string.Join(", ", numbers));
+
+            // Descending order
+            numbers.Sort((a, b) => b.CompareTo(a));
+
+            Console.WriteLine("\nSorted List (Descending):");
+            Console.WriteLine(string.Join(", ", numbers));
+
+            Console.ReadLine();
+        }
+    }
+}
