a95032a33c852f9988524c87b462bbca2f2f0607
upthemaster <upthemaster@gmail.com>
2026-03-08 01:14:06 +0530

    InterfaceSegregationPrinciple.cs is created

---
+++ EXP03/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,49 @@
+﻿//using System.Security.Cryptography;
+
+//namespace ISP
+//{
+
+//    interface IWork
+//    {
+//        void Work();
+//    }
+
+//    interface IEat
+//    {
+//        void Eat();
+//    }
+
+//    // Human can work and eat
+//    class Human : IWork, IEat
+//    {
+//        public void Work()
+//        {
+//            Console.WriteLine("Human working");
+//        }
+
+//        public void Eat()
+//        {
+//            Console.WriteLine("Human eating");
+//        }
+//    }
+
+//    // Robot only works
+//    class Robot : IWork
+//    {
+//        public void Work()
+//        {
+//            Console.WriteLine("Robot working");
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            IWork human = new Human();
+//            human.Work();
+
+//            IWork robot = new Robot();
+//            robot.Work();
+//        }
+//    }
+//}
