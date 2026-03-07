4a6b3579d6315d5fe3f295f646f6eed1e3301768
upthemaster <upthemaster@gmail.com>
2026-03-08 01:11:12 +0530

    ExceptionHandling.cs is created

---
+++ EXP02/ExceptionHandling.cs
@@ -0,0 +1,22 @@
+﻿//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        try
+//        {
+//            int a = 10;
+//            int b = 0;
+
+//            int result = a / b;   // This will cause error
+//            Console.WriteLine(result);
+//        }
+//        catch (DivideByZeroException)
+//        {
+//            Console.WriteLine("Cannot divide by zero!");
+//        }
+
+
+//    }
+//}
