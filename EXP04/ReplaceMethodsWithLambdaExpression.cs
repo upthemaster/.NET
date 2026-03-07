1fbb02b7e7fa03094cfc48db8413d0752ddc9142
upthemaster <upthemaster@gmail.com>
2026-03-08 01:17:38 +0530

    ReplaceMethodsWithLambdaExpression.cs is created

---
+++ EXP04/ReplaceMethodsWithLambdaExpression.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//namespace Replace_Methods_with_lambda_expressions
+
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            // Using built-in Action delegate
+//            Action<string, double> processOrder = null;
+
+//            // Lambda 1 - Generate Bill
+//            processOrder += (customerName, amount) =>
+//            {
+//                Console.WriteLine("Generating bill for " + customerName);
+//                Console.WriteLine("Amount: ₹" + amount);
+//            };
+
+//            // Lambda 2 - Send Email
+//            processOrder += (customerName, amount) =>
+//            {
+//                Console.WriteLine("Sending Email to " + customerName);
+//            };
+
+//            // Lambda 3 - Send SMS
+//            processOrder += (customerName, amount) =>
+//            {
+//                Console.WriteLine("Sending SMS notification...");
+//            };
+
+//            // Calling multicast delegate
+//            processOrder("Aditya", 2500);
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
